e38bf8720546e4c13c6acad78d9a1c083f10d98f
daniou <danioliveras7@gmail.com>
2023-10-01 01:58:35 +0200

    Generado csv de los videos

--- feature_extraction.py
+++ feature_extraction.py
@@ -25,7 +25,7 @@ class FeatureExtraction:
         # Escribir los datos en un archivo CSV
         with open(output_csv, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Archivo', 'Información', 'Transcripción', 'Carpeta'])
+            csv_writer.writerow(['Archivo', 'Transcripción', 'Carpeta'])
             csv_writer.writerows(video_features)
 
 # Ejemplo de uso:
